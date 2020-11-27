c16bd8a1117a99251cb134f05c54940fdf32651f
akash kumar <31354722+akiikr513@users.noreply.github.com>
2020-11-27 11:19:18 +0530

    Update Class_VS_Instance.js
    Class vs. Instance

--- day4/Class_VS_Instance.js
+++ day4/Class_VS_Instance.js
@@ -1,3 +1,23 @@
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
+
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
+
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
+
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+
 function Person(initialAge){
     // Add some more code to run some checks on initialAge
     if(initialAge >= 0) {
@@ -7,10 +27,9 @@ function Person(initialAge){
             initialAge = 0;
             console.log("Age is not valid, setting age to 0.");
        }
-     
-  this.amIOld = (age) => {
+  this.amIOld=function(){
    // Do some computations in here and print out the correct statement to the console
-      if(initialAge < 13) {
+    if(initialAge < 13) {
          console.log("You are young.");
       }else if(initialAge >= 13 && initialAge < 18) {
           console.log("You are a teenager.");
@@ -18,8 +37,24 @@ function Person(initialAge){
           console.log("You are old.");
       }
   };
-   this.yearPasses= (age) =>{
+   this.yearPasses=function(){
           // Increment the age of the person in here
-       initialAge = initialAge+1;
+          initialAge = initialAge+1;
    };
+}
+
+function main() {
+
+var T=parseInt(readLine());
+for(i=0;i<T;i++){
+    var age=parseInt(readLine());
+    var p=new Person(age);
+    p.amIOld();
+    for(j=0;j<3;j++){
+        p.yearPasses();
+        
+    }
+    p.amIOld();
+    console.log("");   
+    }
 }
