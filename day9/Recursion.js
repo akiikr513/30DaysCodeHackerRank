cc8308f93f4a7c0b80328d87dac3f68ea5c229de
akash kumar <31354722+akiikr513@users.noreply.github.com>
2020-12-02 10:14:21 +0530

    Update Recursion.js
    day 9

--- day9/Recursion.js
+++ day9/Recursion.js
@@ -1,8 +1,47 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the factorial function below.
 function factorial(n) {
-  // Complete this function
-  var result = 1;
-  for (i = n; i >= 1; i--) {
+var result = 1;
+  for (var i = n; i >= 1; i--) {
     result *= i
   }
   return result
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let result = factorial(n);
+
+    ws.write(result + "\n");
+
+    ws.end();
 }
