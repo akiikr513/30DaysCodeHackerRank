6ec708905dbc6263dbc907c8c985df9586adf809
akash kumar <31354722+akiikr513@users.noreply.github.com>
2020-12-03 09:53:07 +0530

    Update Binary_Numbers.js
    day 10

--- day_10/Binary_Numbers.js
+++ day_10/Binary_Numbers.js
@@ -1,43 +1,44 @@
-function main() {
-  var n = parseInt(readLine());
-  var binary = [];
+'use strict';
 
-  while (n > 0) {
-    binary.push(n % 2)
-    n = Math.floor(n / 2)
-  }
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
 
-  binary = binary.reverse()
+let inputString = '';
+let currentLine = 0;
 
-  var ones = 0;;
-  var max = 0;
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
 
-  for (i = 0; i < binary.length; i++) {
-    if (binary[i] === 1) {
-      ones += 1;
-    } else if (binary[i] === 0) {
-      if (ones > max) max = ones;
-      ones = 0;
-    }
-  }
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
 
-  if (ones > max) max = ones
-  console.log(max)
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
 }
 
-// var n = parseInt(readLine());
-// const countArray=[];
-// let count=0;
-// let inputBinary = n.toString(2).split('');
-// inputBinary.forEach((digit, index)=>{
-//   if(digit==='1'){
-//     count++;
-//     if(index===inputBinary.length-1){
-//       countArray.push(count);
-//     }
-//   }else{
-//     countArray.push(count);
-//     count=0;
-//   }
-// });
-// console.log(countArray.sort((a,b)=>b-a)[0]);
+
+
+function main() {
+    const n = parseInt(readLine(), 10);
+    const countArray=[];
+    let count=0;
+    let inputBinary = n.toString(2).split('');
+    inputBinary.forEach((digit, index)=>{
+        if(digit==='1'){
+            count++;
+        if(index===inputBinary.length-1){
+            countArray.push(count);
+        }
+        }else{
+            countArray.push(count);
+            count=0;
+        }
+    });
+    console.log(countArray.sort((a,b)=>b-a)[0]);
+}
