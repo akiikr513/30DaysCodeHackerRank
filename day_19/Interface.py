46f15d53fbad2b79ae3f3d290bc5a2d525d652b6
akash kumar <31354722+akiikr513@users.noreply.github.com>
2020-12-12 13:03:26 +0530

    Update and rename Interface.js to Interface.py
    Day 19

--- day_19/Interface.py
+++ day_19/Interface.py
@@ -1,4 +1,24 @@
-// class AdvancedArithmetic {
+class AdvancedArithmetic(object):
+    def divisorSum(n):
+        raise NotImplementedError
+
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        sum = 0
+        for i in range(1, n//2 + 1):
+            if n % i == 0:
+                sum += i
+        return sum + n
+
+
+n = int(input())
+my_calculator = Calculator()
+s = my_calculator.divisorSum(n)
+print("I implemented: " + type(my_calculator).__bases__[0].__name__)
+print(s)
+
+
+"""// class AdvancedArithmetic {
 //   divisor(n) {
 //     return n || 0;
 //   };
@@ -30,4 +50,4 @@
 //   console.log("I implemented: AdvancedArithmetic\n" + sum); 
 // }
 
-// Solution();
+// Solution();"""
