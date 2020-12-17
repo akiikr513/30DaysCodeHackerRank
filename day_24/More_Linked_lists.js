6f217e05e3488fc00bfa79ea80b7e6ab2561d35b
akash kumar <31354722+akiikr513@users.noreply.github.com>
2020-12-17 11:35:38 +0530

    Update More_Linked_lists.js
    day 24

--- day_24/More_Linked_lists.js
+++ day_24/More_Linked_lists.js
@@ -1,14 +1,41 @@
-// this.removeDuplicates=function(head){
-//     //Write your code here
-//     let prev = head;
+process.stdin.resume();
+process.stdin.setEncoding('ascii');
 
-//     while (prev) {
-//         let next = prev.next;
+var input_stdin = "";
+var input_stdin_array = "";
+var input_currentline = 0;
 
-//         (next && prev.data == next.data)
-//             ? (prev.next = next.next)
-//             : (prev = prev.next);
-//     }
+process.stdin.on('data', function (data) {
+    input_stdin += data;
+});
 
-//     return head;
-// }
+process.stdin.on('end', function () {
+    input_stdin_array = input_stdin.split("\n");
+    main();    
+});
+function readLine() {
+    return input_stdin_array[input_currentline++];
+}
+function Node(data){
+    this.data=data;
+    this.next=null;
+}
+function Solution(){
+
+    this.removeDuplicates=function(head){
+      //Write your code here
+      let prev = head;
+
+    while (prev) {
+        let next = prev.next;
+
+        (next && prev.data == next.data)
+            ? (prev.next = next.next)
+            : (prev = prev.next);
+    }
+
+    return head;
+
+    }
+
+	this.insert=function(head,data){
