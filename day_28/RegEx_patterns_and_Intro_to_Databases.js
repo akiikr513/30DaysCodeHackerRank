a0dacbb89ff4271a6c40ba7927aac901c00b818d
akash kumar <31354722+akiikr513@users.noreply.github.com>
2020-12-21 15:40:12 +0530

    Update RegEx_patterns_and_Intro_to_Databases.js
    day_28

--- day_28/RegEx_patterns_and_Intro_to_Databases.js
+++ day_28/RegEx_patterns_and_Intro_to_Databases.js
@@ -1,11 +1,39 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+
+
 function main() {
-    var N = parseInt(readLine());
-    var names = []
-    for(var a0 = 0; a0 < N; a0++){
-        var firstName_temp = readLine().split(' ');
-        var firstName = firstName_temp[0];
-        var emailID = firstName_temp[1];
-        if(emailID.match(/@gmail\.com$/)) {
+    const N = parseInt(readLine(), 10);
+    let names=[];
+    for (let NItr = 0; NItr < N; NItr++) {
+        const firstNameEmailID = readLine().split(' ');
+
+        const firstName = firstNameEmailID[0];
+
+        const emailID = firstNameEmailID[1];
+    if(emailID.match(/@gmail\.com$/)) {
             names.push(firstName)
         }
     }
